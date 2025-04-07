bb8390bb1f7c69808a99a727d1e9743eed4b7c81
denchikLOG <deniss.melnikov@gmail.com>
2025-04-07 11:39:18 +0300

    Update Module_palgad.py

--- Module_palgad.py
+++ Module_palgad.py
@@ -12,7 +12,7 @@ def Lisa_andmd(p: list,i: list):
             i.appned(palk)
             print("andmet on lisatud")
     except:
-        print("Kirjuta ainult t�htede kasutades")
+        print("Kirjuta ainult tähtede kasutades")
 
 from Module_palgad import *
 palgad=[1200,2500,750,395,1300]
@@ -22,10 +22,33 @@ while True:
     print("andmed:")
     print(inimesed)
     print(palgad)
-    print("vajuta:\n1-Andmete lisameks\n2-Andmete kustutamiseks nime j�rgi")
+    print("vajuta:\n1-Andmete lisameks\n2-Andmete kustutamiseks nime järgi")
     v=int(input())
     if v==1:
         k=int(input("mitu inimest? "))
         for i in range(k):
-            Lisa_andmed(palgad,inimesed)
+            lisa_andmed(palgad,inimesed)
+
+
+def sorteerimine(p: list, i: list)-> any:
+    v=input("Vali märk: > (kasvav) vöi (kahanev)")#try except? koos while true'ga
+    for i in range(0, len(i)):
+        for i in range(n, len(i)):
+            if eval(str(p[n])+v+str(p[m])):
+                p[n],p[m]=p[m],p[n]
+                i[n],i[m]=i[m],i[n]
+    return p,i
+
+def võrdsed_palgad(p:list, i: list):
+    hulk=set(p)
+    print(hulk)
+    for palk in hulk:
+        k=p.count(palk)
+        if k>1:
+            print(f"Palk {palk}")
+            ind=p.index(palk)
+            for j in range(k):
+                ind=p.index(palk,ind)
+                print(f"saab kätte {i[ind]}")
+                ind+=1
 
